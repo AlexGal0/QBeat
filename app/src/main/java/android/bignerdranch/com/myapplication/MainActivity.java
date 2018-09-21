5ac946a8ffb9beb92cd93eee667485d3b5af55f5
Marlon Estupinan <maestupinan2@poligran.edu.co>
2018-09-20 19:33:02 -0500

    Delete all not used files

--- app/src/main/java/android/bignerdranch/com/myapplication/MainActivity.java
+++ app/src/main/java/android/bignerdranch/com/myapplication/MainActivity.java
@@ -2,25 +2,18 @@ package android.bignerdranch.com.myapplication;
 
 import android.content.res.ColorStateList;
 import android.graphics.Color;
-import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Display;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
