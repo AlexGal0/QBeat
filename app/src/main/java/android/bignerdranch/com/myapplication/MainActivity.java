c9b42da68345fc218c08675e0834d447504420d4
edfbarong18 <edwardbaron99@gmail.com>
2018-09-20 20:46:45 -0500

    Borrar linea LOG.

--- app/src/main/java/android/bignerdranch/com/myapplication/MainActivity.java
+++ app/src/main/java/android/bignerdranch/com/myapplication/MainActivity.java
@@ -28,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("HELLO", "HELLOTHERE");
 
 
         mViewPager = new ViewPageFragment(this);
