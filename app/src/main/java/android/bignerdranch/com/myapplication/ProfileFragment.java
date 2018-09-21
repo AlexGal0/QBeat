a44e8156006d06fcfd6843a093a05908be8d4e3d
edfbarong18 <edwardbaron99@gmail.com>
2018-09-20 20:53:39 -0500

    Tag de Perfil en ProfileFragment.

--- app/src/main/java/android/bignerdranch/com/myapplication/ProfileFragment.java
+++ app/src/main/java/android/bignerdranch/com/myapplication/ProfileFragment.java
@@ -3,6 +3,7 @@ package android.bignerdranch.com.myapplication;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ public class ProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        Log.i("Perfil", "Este es el perfil.");
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 }
