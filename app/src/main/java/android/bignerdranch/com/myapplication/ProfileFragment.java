3063a4e6adefc8d4c24dda0dec5101ad5ff0c423
edfbarong18 <edwardbaron99@gmail.com>
2018-09-21 18:37:59 -0500

    Aplicacion/Uso de la clase perfil con el fragmento.

--- app/src/main/java/android/bignerdranch/com/myapplication/ProfileFragment.java
+++ app/src/main/java/android/bignerdranch/com/myapplication/ProfileFragment.java
@@ -7,11 +7,25 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class ProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Usuario user = new Usuario("Carlos Satanas");
+        user.addExperience(50);
+
+        TextView nombre = container.findViewById(R.id.username_profile);
+        nombre.setText(user.getName());
+
+        ProgressBar bar = container.findViewById(R.id.progressbar_profile);
+        bar.setProgress((int) user.getCurrentExperience());
+
+        return v;
+
     }
 }
