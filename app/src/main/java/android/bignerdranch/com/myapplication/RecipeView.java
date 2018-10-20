4fe8f3e4dd0a9d2dc81d87c0e73a5e022680f3b0
Marlon Estupinan <maestupinan2@poligran.edu.co>
2018-10-19 19:51:18 -0500

    Fixed some bugs Add option to delete a recipe

--- app/src/main/java/android/bignerdranch/com/myapplication/RecipeView.java
+++ app/src/main/java/android/bignerdranch/com/myapplication/RecipeView.java
@@ -1,10 +1,17 @@
 package android.bignerdranch.com.myapplication;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -16,13 +23,13 @@ public class RecipeView extends FragmentActivity {
 
         int index = getIntent().getIntExtra(MyRecipeFragment.TAG_RECIPE, -1);
 
-        Receta receta = DataBase.getDataBase().getListReceta(User.id).get(index);
+        final Receta receta = DataBase.getDataBase().getListReceta(User.id).get(index);
 
         ArrayList<Ingrediente>  listIngredients = receta.getIngredientes();
         ArrayList<Paso>         listStep       = receta.getPasos();
 
 
-        TextView name = findViewById(R.id.name_recipe_view);
+        final TextView name = findViewById(R.id.name_recipe_view);
         TextView description = findViewById(R.id.description_recipe_view);
 
         LinearLayout ingredientsContainer   = findViewById(R.id.ingredients_container_recipe_view);
@@ -44,5 +51,36 @@ public class RecipeView extends FragmentActivity {
             stepContainer.addView(text);
         }
 
+
+        final Button delete = findViewById(R.id.delete_recipe_button);
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                delete.setEnabled(false);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+
+                builder.setTitle("¿Desea eliminar la receta : " + receta.getName() + "?");
+                builder.setMessage("La receta se va a eliminar para siempre y no se podra recuperar");
+                builder.setCancelable(false);
+                builder.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        DataBase.getDataBase().removeRecipe(receta);
+                        Toast.makeText(view.getContext(), "La receta ha sido eliminada correctamente", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        delete.setEnabled(true);
+                    }
+                });
+
+                builder.show();
+            }
+        });
     }
 }
